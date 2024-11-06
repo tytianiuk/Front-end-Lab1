06190501522f3bc4cac310279d1f674753b0383c
Tytianiuk Artem <tytianiuk@gmail.com>
2024-11-06 13:14:21 +0200

    Implement second task
    - update index.html
    - add styles for container of images
    - implement second task

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-import { addEventListener } from './utils.js';
+import { addEventListener, addEventListenersForButtons } from './utils.js';
 
 const variantElement = document.getElementById('variant-element');
 const nextVariantElement = document.querySelector('.next-variant-element');
+const imagesContainer = document.getElementById('container');
+const buttons = document.querySelectorAll('button');
+
+addEventListenersForButtons(buttons, imagesContainer);
 
 addEventListener(variantElement);
 addEventListener(nextVariantElement);
--- utils.js
+++ utils.js
@@ -18,4 +18,45 @@ const addEventListener = (element) => {
   });
 };
 
-export { addEventListener };
+const addImage = (container) => {
+  const images = container.getElementsByTagName('img');
+  const newImage = images[0].cloneNode();
+  container.appendChild(newImage);
+};
+
+const increaseSize = (container) => {
+  const images = container.getElementsByTagName('img');
+  if (images.length > 0) {
+    const lastImage = images[images.length - 1];
+    const currentWidth = lastImage.clientWidth;
+    lastImage.style.width = currentWidth + 100 + 'px';
+  }
+};
+
+const decreaseSize = (container) => {
+  const images = container.getElementsByTagName('img');
+  if (images.length > 0) {
+    const lastImage = images[images.length - 1];
+    const currentWidth = lastImage.clientWidth;
+    if (currentWidth > 50) {
+      lastImage.style.width = currentWidth - 100 + 'px';
+    }
+  }
+};
+
+const removeImage = (container) => {
+  const images = container.getElementsByTagName('img');
+  if (images.length > 1) {
+    container.removeChild(images[images.length - 1]);
+  }
+};
+
+const imageUtils = [addImage, increaseSize, decreaseSize, removeImage];
+
+const addEventListenersForButtons = (buttons, container) => {
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', () => imageUtils[i](container));
+  }
+};
+
+export { addEventListener, addEventListenersForButtons };
